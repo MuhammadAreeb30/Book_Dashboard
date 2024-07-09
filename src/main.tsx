eba7f5d9b328e014b83db3f6708b259354c24c40
MuhammadAreeb30 <almostevil55@gmail.com>
2024-07-09 21:13:22 +0500

    login page

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Router } from "./router.tsx";
 import { RouterProvider } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={Router} />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
   </React.StrictMode>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import {
@@ -10,8 +11,61 @@ import {
 } from "../components/ui/card";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
+import { useRef, useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { LoaderCircle } from "lucide-react";
+import axios from "axios";
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  const userLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const user = {
+        email: emailRef.current?.value,
+        password: passwordRef.current?.value,
+      };
+      console.log(user, "user");
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_AUTH_URI}login`,
+        user
+      );
+      console.log(response, "response");
+
+      if (response.status === 200) {
+        setIsLoading(false);
+        navigate("/dashboard/home");
+        return toast.success("Login Successfully", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      setIsLoading(false);
+      toast.error("Something went wrong", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
   return (
     <section className="h-screen flex justify-center items-center">
       <Card className="w-full max-w-sm shadow-md">
@@ -30,6 +84,7 @@ const Login = () => {
               placeholder="m@example.com"
               required
               className="focus-visible:ring-main"
+              ref={emailRef}
             />
           </div>
           <div className="grid gap-2">
@@ -39,12 +94,25 @@ const Login = () => {
               type="password"
               required
               className="focus-visible:ring-main"
+              ref={passwordRef}
             />
           </div>
         </CardContent>
         <CardFooter className="flex flex-col gap-4">
-          <Button className="w-full bg-main hover:bg-main hover:opacity-[0.90]">
-            Sign in
+          <Button
+            className="w-full bg-main hover:bg-main hover:opacity-[0.90]"
+            type="submit"
+            onClick={userLogin}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <>
+                <LoaderCircle className="animate-spin" />{" "}
+                <span className="ml-2">Sign in</span>
+              </>
+            ) : (
+              "Sign in"
+            )}
           </Button>
           <CardDescription>
             Don't have an account?{" "}
